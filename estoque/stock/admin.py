1fe5bd3499421277f392eef73ba5a556f148827e
Mariana Cossetti Dalfior <99911510+MCossetti@users.noreply.github.com>
2024-11-11 16:21:04 -0300

    Criando template básico

--- estoque/stock/admin.py
+++ estoque/stock/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Estoque, EstoqueProduto
 
+
+class EstoqueProdutoInLine(admin.TabularInline):
+    model = EstoqueProduto
+    extra = 0
+
 @admin.register(Estoque)
 class ProdutoAdmin(admin.ModelAdmin):
+    inlines = (EstoqueProdutoInLine,)
     list_display=('__str__', 'usuario', 'movimento')
     search_fields=('id',)
     list_filter=('usuario',)
