4f94fc9ac3b5e6e30184dd80a59a4d54a3ba2ce5
Mariana Cossetti Dalfior <99911510+MCossetti@users.noreply.github.com>
2024-11-14 10:37:39 -0300

    Criação do model de Categoria para produtos

--- estoque/produto/models.py
+++ estoque/produto/models.py
@@ -5,8 +5,10 @@ class Produto(models.Model):
     nome = models.CharField('Nome:', max_length=100)
     descricao = models.CharField('Descrição:', max_length=200, blank=True, null=True)
     preco = models.DecimalField('Preço:', max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey('Categoria', on_delete=models.SET_NULL, null=True, blank=True)
     estoque = models.IntegerField('Estoque Atual:')
     estoque_minimo = models.PositiveIntegerField('Estoque Mínimo:', default=0)
+    
 
     class Meta:
         ordering = ('nome',)
@@ -23,3 +25,12 @@ class Produto(models.Model):
             'produto': self.nome,
             'estoque':self.estoque,
         }
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=75)
+
+    class Meta:
+        ordering = ('nome',)
+
+    def __str__(self):
+        return self.nome
