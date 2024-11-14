251d59c167d640a974893e93ab426327b0021979
Mariana Cossetti Dalfior <99911510+MCossetti@users.noreply.github.com>
2024-11-13 22:33:02 -0300

    Criando campo de busca no estoque

--- estoque/produto/views.py
+++ estoque/produto/views.py
@@ -4,13 +4,14 @@ from django.views.generic import CreateView, UpdateView
 from .models import Produto
 from .forms import ProdutoForm
 
-
 def produto_list(request):
     template_name = 'produto_list.html'
     objects = Produto.objects.all()
+    search = request.GET.get('search')
+    if search:
+        objects = objects.filter(nome__icontains=search)
     context = {'object_list': objects}
     return render(request, template_name, context)
-
 
 def produto_detail(request, pk):
     template_name = 'produto_detail.html'
