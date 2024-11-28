b58f48ef4ad72643c804218b7cc1780d9d67b92a
Mariana Cossetti Dalfior <99911510+MCossetti@users.noreply.github.com>
2024-11-28 12:44:44 -0300

    Criação de restaurar senha

--- estoque/settings.py
+++ estoque/settings.py
@@ -127,4 +127,10 @@ LOGOUT_URL = 'logout'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# EMAIL_HOST= config('EMAIL_HOST')
+# EMAIL_PORT= config('EMAIL_PORT')
+# EMAIL_USE_TLS= config('EMAIL_USE_TLS')
+# EMAIL_HOST_USER= config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD')
