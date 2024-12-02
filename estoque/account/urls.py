2d5ddeb4a638ce4855f530ec3a6e72d90d2d08b2
Mariana Cossetti Dalfior <99911510+MCossetti@users.noreply.github.com>
2024-12-02 00:30:03 -0300

    Criação do design

--- estoque/account/urls.py
+++ estoque/account/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('password_change/', v.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', v.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
-    path('password_reset/', v.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', v.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/', v.PasswordResetView.as_view(success_url='/account/login/'), name='password_reset'),
     path('reset/<uidb64>/<token>', v.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', v.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
