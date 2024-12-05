b81073792acda832dfbe85dfdd43e7f9405bf477
Mariana Cossetti Dalfior <99911510+MCossetti@users.noreply.github.com>
2024-12-05 11:30:05 -0300

    Alteração de urls

--- estoque/core/urls.py
+++ estoque/core/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from estoque.core import views as v
 from django.views.generic.base import RedirectView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 app_name = 'core'
 
+@login_required(login_url='account/login/')
+def index_redirect(request):
+    return redirect('/produto')
+
+
 urlpatterns=[
-    path('', RedirectView.as_view(url='produto', permanent=False), name='index')
+    path('', index_redirect, name='index'),
 ]
